7b8a75f44f4c9db28d0f5379df6fb67e74d515da
sachavent <ventribout@et.esiea.fr>
2015-11-17 09:06:34 +0100

    Ajout d'une page basique
    Page html basique : http://localhost/Symfony/web/app_dev.php/platform

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -7,12 +7,38 @@ namespace OC\PlatformBundle\Controller;
 // N'oubliez pas ce use :
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdvertController extends Controller
 {
+   public function viewAction($id)
+  {
+	 return $this->render(
+      'OCPlatformBundle:Advert:view.html.twig',
+      array('id'  => $id)
+    );
+  }
   public function indexAction()
   {
     $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig');
     return new Response($content);
   }
+  
+  public function addAction(Request $request)
+  {
+    $session = $request->getSession();
+    
+    // Bien sûr, cette méthode devra réellement ajouter l'annonce
+    
+    // Mais faisons comme si c'était le cas
+    $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
+
+    // Le « flashBag » est ce qui contient les messages flash dans la session
+    // Il peut bien sûr contenir plusieurs messages :
+    $session->getFlashBag()->add('info', 'Oui oui, il est bien enregistré !');
+
+    // Puis on redirige vers la page de visualisation de cette annonce
+    return $this->redirect($this->generateUrl('oc_platform_view', array('id' => 5)));
+  }
+
 }
