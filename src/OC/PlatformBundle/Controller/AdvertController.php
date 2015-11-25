cd3970b6e19ba75b11850407b1268f340a163950
sachavent <ventribout@et.esiea.fr>
2015-11-25 18:34:31 +0100

    Ajout / Suppression Article
    Gestion de la bdd

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -7,9 +7,12 @@ namespace OC\PlatformBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use  OC\PlatformBundle\Entity\Advert;
+use OC\PlatformBundle\Entity\Image;
 
 class AdvertController extends Controller
 {
+// Fonction page d'accueuil
   public function indexAction($page)
   {
     // On ne sait pas combien de pages il y a
@@ -40,47 +43,86 @@ class AdvertController extends Controller
         'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
         'date'    => new \Datetime())
     );
-
+	
     // Et modifiez le 2nd argument pour injecter notre liste
     return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
       'listAdverts' => $listAdverts
     ));
   }
-
+// Fonction voir les annonces
   public function viewAction($id)
   {
-	// PERMET DE VOIR UNE ANNONCE MAIS A MODIFIER PAR LA BDD
-    $advert = array(
-      'title'   => 'Recherche développpeur Symfony2',
-      'id'      => $id,
-      'author'  => 'Alexandre',
-      'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
-      'date'    => new \Datetime()
-    );
+    // On récupère le repository
+    $repository = $this->getDoctrine()
+      ->getManager()
+      ->getRepository('OCPlatformBundle:Advert')
+    ;
 
+    // On récupère l'entité correspondante à l'id $id
+    $advert = $repository->find($id);
+
+    // $advert est donc une instance de OC\PlatformBundle\Entity\Advert
+    // ou null si l'id $id  n'existe pas, d'où ce if :
+    if (null === $advert) {
+      throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+    }
+
+    // Le render ne change pas, on passait avant un tableau, maintenant un objet
     return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
       'advert' => $advert
     ));
   }
-
+// Fonction ajouter article
   public function addAction(Request $request)
   {
-    // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
+	/*
+	// On récupère le service
+    $antispam = $this->container->get('oc_platform.antispam');
 
-    // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+    // Je pars du principe que $text contient le texte d'un message quelconque
+    $text = '...';
+    if ($antispam->isSpam($text)) {
+      throw new \Exception('Votre message a été détecté comme spam !');
+    }
+    // Ici le message n'est pas un spam 
+	*/
+  
+    // Création de l'entité Advert
+    $advert = new Advert();
+    $advert->setTitle('Recherche développeur Symfony2.');
+    $advert->setAuthor('Alexandre');
+    $advert->setContent("Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…");
+
+    // Création de l'entité Image
+    $image = new Image();
+    $image->setUrl('http://sdz-upload.s3.amazonaws.com/prod/upload/job-de-reve.jpg');
+    $image->setAlt('Job de rêve');
+
+    // On lie l'image à l'annonce
+    $advert->setImage($image);
+
+    // On récupère l'EntityManager
+    $em = $this->getDoctrine()->getManager();
+
+    // Étape 1 : On « persiste » l'entité
+    $em->persist($advert);
+
+    // Étape 1 bis : si on n'avait pas défini le cascade={"persist"},
+    // on devrait persister à la main l'entité $image
+    // $em->persist($image);
+
+    // Étape 2 : On déclenche l'enregistrement
+    $em->flush();
+
+    // Reste de la méthode qu'on avait déjà écrit
     if ($request->isMethod('POST')) {
-      // Ici, on s'occupera de la création et de la gestion du formulaire
-
       $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
-
-      // Puis on redirige vers la page de visualisation de cettte annonce
-      return $this->redirect($this->generateUrl('oc_platform_view', array('id' => 5)));
+      return $this->redirect($this->generateUrl('oc_platform_view', array('id' => $advert->getId())));
     }
 
-    // Si on n'est pas en POST, alors on affiche le formulaire
     return $this->render('OCPlatformBundle:Advert:add.html.twig');
   }
-
+// Fonction modification articles
   public function editAction($id, Request $request)
   {
     // CE QUON VOIT QUAND ON EDITE UN ARTICLE
@@ -104,6 +146,21 @@ class AdvertController extends Controller
     // Ici, on récupérera l'annonce correspondant à $id
 
     // Ici, on gérera la suppression de l'annonce en question
+	
+    $em = $this->getDoctrine()->getManager();
+
+    // On récupère l'annonce $id
+    $advert = $em->getRepository('OCPlatformBundle:Advert')->find($id);
+
+    if (null === $advert) {
+      throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+    }
+
+	$em->remove($advert);
+	
+	// commit
+	$em->flush();
+
 
     return $this->render('OCPlatformBundle:Advert:delete.html.twig');
   }
